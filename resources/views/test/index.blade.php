3a83f4155b23c53c3c83101bd924f65b59616d03
Nopparat412 <fangdne.05@gmail.com>
2023-11-20 12:03:12 +0700

    Initial commit

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="gallery/ant"> ant <img src={{$ant}} alt="">
+    <a href="gallery/cat"> cat <img src={{$cat}} alt="">
+    <a href="gallery/bird"> bird<img src={{$bird}} alt="">
+    <a href="gallery/god"> god <img src={{$god}} alt="">
+    <a href="gallery/spider"> spider <img src={{$spider}} alt="">
+</body>
+</html>
