561e28222369f34a4755d71c0ac9838c927406e3
Nopparat412 <fangdne.05@gmail.com>
2023-12-04 12:05:51 +0700

    week4

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -6,10 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="gallery/ant"> ant <img src={{$ant}} alt="">
+    <a href="gallery/ant"> ant <img src={{$ant}} alt="" >
     <a href="gallery/cat"> cat <img src={{$cat}} alt="">
     <a href="gallery/bird"> bird<img src={{$bird}} alt="">
     <a href="gallery/god"> god <img src={{$god}} alt="">
     <a href="gallery/spider"> spider <img src={{$spider}} alt="">
 </body>
-</html>
+</html> 
